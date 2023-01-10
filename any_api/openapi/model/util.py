ecdc56b24c2be6f1464f54be48bf59747f2999d4
so1n <qaz6803609@163.com>
2023-01-11 00:04:25 +0800

    Modify, Refactor, remove http param type param

--- any_api/openapi/model/util.py
+++ any_api/openapi/model/util.py
@@ -3,6 +3,4 @@ from typing_extensions import Literal
 SecurityLiteral = Literal["apiKey", "http", "oauth2", "openIdConnect"]
 SecurityHttpParamTypeLiteral = Literal["query", "header", "cookie"]
 HttpMethodLiteral = Literal["get", "post", "head", "options", "delete", "put", "trace", "patch"]
-HttpParamTypeLiteral = Literal[
-    "body", "cookie", "file", "form", "header", "json", "path", "query", "multiform", "multiquery", "multiheader"
-]
+HttpParamTypeLiteral = Literal["body", "cookie", "file", "form", "header", "json", "path", "query", "multiform"]
